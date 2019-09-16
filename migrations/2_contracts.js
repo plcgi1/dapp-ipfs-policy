7c9053b11092e489afd5520b6f959ca300993182
Nosau Aliaksandr <plcgi1@gmail.com>
2019-09-16 16:22:33 +0300

    Additional info to logger refun

--- migrations/2_contracts.js
+++ migrations/2_contracts.js
@@ -1,5 +1,5 @@
 var ERC721WithPolicy = artifacts.require("./ERC721WithPolicy.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ERC721WithPolicy);
+  deployer.deploy(ERC721WithPolicy, 'one', 'sym');
 };
